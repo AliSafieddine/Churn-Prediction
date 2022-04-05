45c73f18d1e4dbc811745798121df3e75b0d5326
Marios Eftychiou <marios.eftychiou@student.kuleuven.be>
2022-04-05 10:50:26 +0300

    churnenv

--- churnenv/Scripts/activate.bat
+++ churnenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ali\Desktop\Churn Prediction\Churn-Prediction\churnenv
+set VIRTUAL_ENV=C:\Users\35799\Desktop\KU Leuven\Semester 2\Advanced Analytics in a Big Data World\Project\Churn-Prediction\churnenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(churnenv) 
 
 :END
 if defined _OLD_CODEPAGE (
